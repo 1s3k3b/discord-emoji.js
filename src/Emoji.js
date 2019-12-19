6d804431d4be797c4b044905068323d9c2654f81
1s3k3b <56838314+1s3k3b@users.noreply.github.com>
2019-12-19 21:35:03 +0100

    Create Emoji.js

---
+++ src/Emoji.js
@@ -0,0 +1,26 @@
+const util = require("./util.js");
+
+module.exports = class Emoji {
+    constructor(data) {
+        this.id = data.id;
+        this.name = data.title;
+        this.slug = data.slug;
+        this.imageURL = data.image;
+        this.siteURL = "https://discordemoji.com/emoji/" + this.slug;
+        this.description = data.description;
+        this.category = {
+            name: util.getCategoryName(data.category),
+            id: data.category,
+            toString() {
+                return util.getCategoryName(data.category);
+            },
+            valueOf() {
+                return data.category;
+            }
+        };
+        this.license = data.license;
+        this.source = data.source;
+        this.favorites = data.faves;
+        this.author = data.submitted_by;
+    }
+}
