6e8fbb7feb846c01fe1089739c8fd7d527ffef74
1s3k3b <56838314+1s3k3b@users.noreply.github.com>
2019-12-19 18:41:20 +0100

    Update util.js

--- src/util.js
+++ src/util.js
@@ -1,4 +1,10 @@
-// even though these are in the main file in the property "util", these should only be used with the expected data types because these methods aren't designed for user input.
+// even though these are in the main file under the property "util", these should only be used with the expected data types because these methods aren't designed for user input.
 module.exports = {
-    search: (str, categories) => {}
+    search: (str, categories) => {
+        for (const id of Object.keys(categories)) {
+            const name = categories[id];
+            if (str.replace(/ +/g, "_").replace(/\W/g, "_") === name.replace(/ +/g, "_").replace(/\W/g, "_")) return Number(id);
+        }
+        return void console.warn("Invalid category name " + str);
+    }
 };
