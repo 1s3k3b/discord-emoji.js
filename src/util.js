e9b588f47f07240166328d994848b0c3f0dad382
1s3k3b <56838314+1s3k3b@users.noreply.github.com>
2019-12-19 21:24:48 +0100

    Update util.js

--- src/util.js
+++ src/util.js
@@ -1,9 +1,10 @@
 // even though these are in the main file under the property "util", these should only be used with the expected data types because these methods aren't designed for user input.
 module.exports = {
+    findStr: (x, y) => x.replace(/ +/g, "").replace(/\W/g, "").toLowerCase() === y.replace(/ +/g, "").replace(/\W/g, "").toLowerCase(),
     search: (str, categories) => {
         for (const id of Object.keys(categories)) {
             const name = categories[id];
-            if (str.replace(/ +/g, "").replace(/\W/g, "").toLowerCase() === name.replace(/ +/g, "").replace(/\W/g, "").toLowerCase()) return Number(id);
+            if (module.exports.findStr(str, name)) return Number(id);
         }
         return void console.warn("Invalid category name " + str);
     }
