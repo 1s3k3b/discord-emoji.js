785c8bafd8d117f669806ee620fc9bbbc4a31289
1s3k3b <56838314+1s3k3b@users.noreply.github.com>
2019-12-19 21:41:02 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,12 +10,14 @@ module.exports = {
         categories: "https://discordemoji.com/api?request=categories",
         packs: "https://discordemoji.com/api/packs"
     },
+    util,
     getSiteData: () => fetch(module.exports.urls.stats).then(d => d.json()),
     getEmojis: async (category = "all", map) => {
         category = resolver.resolveCategories(category);
         map = map ? String(map).toLowerCase().trim() : undefined;
-        return (await (await fetch(module.exports.urls.allEmojis).then(async x => (await x.json()).filter(y => category.some(z => y.category === z)).map(r => !map ? r : (r[map] || r)))));
+        return (await (await fetch(module.exports.urls.allEmojis).then(async x => (await x.json()).filter(y => category.some(z => y.category === z)).map(r => !map ? r : (r[map] || r))))).map(x => new Emoji(x));
     },
-    findEmoji: async (name) => (await module.exports.getEmojis()).find(x => util.findStr(name, x.name)),
-    getEmoji
+    findEmoji: async name => (await module.exports.getEmojis()).find(x => util.findStr(name, x.name)),
+    getEmoji: async id => (await module.exports.getEmojis()).find(x => x.id === Number(String(id).replace(/\W/g, ""))),
+    getAuthorEmojis: async name => (await module.exports.getEmojis()).filter(x => util.findStr(name, x.author))
 };
