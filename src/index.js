13f211be24c1bc34880566e1c366f7cf413112a2
1s3k3b <56838314+1s3k3b@users.noreply.github.com>
2019-12-19 17:56:37 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@ const fetch = require("node-fetch");
 
 module.exports = {
     getSiteData: () => fetch("https://discordemoji.com/api?request=stats").then(d => d.json()),
-    getEmojis: (category = "all") => fetch()
+    getEmojis: async (category = "all", map = "none") => {
+        category = resolver.resolveCategory(category);
+        map = resolver.resolveMapType(map);
+        return fetch("https://discordemoji.com/api/").then(async x => (await x.json()).filter(y => category.some(z => y.category === z)).map(r => map === "none" ? r : (r[map] || r));
+    }
 };
