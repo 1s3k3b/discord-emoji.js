b251621a4351df8eb63316f941feb446a227d803
1s3k3b <56838314+1s3k3b@users.noreply.github.com>
2019-12-19 18:35:27 +0100

    Create util.js

---
+++ src/util.js
@@ -0,0 +1,4 @@
+// even though these are in the main file in the property "util", these should only be used with the expected data types because these methods aren't designed for user input.
+module.exports = {
+    search: (str, categories) => {}
+};
