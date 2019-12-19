6b205ae37730804a4947258475ce54dffe645765
1s3k3b <56838314+1s3k3b@users.noreply.github.com>
2019-12-19 17:52:15 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+const fetch = require("node-fetch");
+
 module.exports = {
-    
+    getSiteData: () => fetch("https://discordemoji.com/api?request=stats").then(d => d.json()),
+    getEmojis: (category = "all") => fetch()
 };
